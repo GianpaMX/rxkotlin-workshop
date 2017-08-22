3bc64410c31f35f3d9b236b4f6e782d3ff285f84
Juan Pablo "Gianpa" Villa <juan64@gmail.com>
2017-08-22 11:15:41 -0500

    shouldDownloadAllUrls

--- src/test/java/io/github/gianpamx/rxkotlin/R21_FlatMap.kt
+++ src/test/java/io/github/gianpamx/rxkotlin/R21_FlatMap.kt
@@ -2,28 +2,42 @@ package io.github.gianpamx.rxkotlin
 
 import io.github.gianpamx.rxkotlin.util.UrlDownloader
 import io.github.gianpamx.rxkotlin.util.Urls
-import io.reactivex.Flowable
 import io.reactivex.schedulers.Schedulers
-import org.assertj.core.api.Assertions
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
+import java.net.URI
 
 class R21_FlatMap {
     @Test
     fun shouldDownloadAllUrlsInArbitraryOrder() {
-        val all = Urls.all()
+        val urls = Urls.all()
 
-        val urls: Flowable<String> = all
-                .flatMap { url ->
-                    UrlDownloader
-                            .download(url)
-                            .subscribeOn(Schedulers.io())
-                }
 
-        var bodies: List<String>? = urls
+        var bodies = urls
+                .flatMap { url -> UrlDownloader.downloadAsync(url, Schedulers.io()) }
                 .toList()
                 .blockingGet()
 
-        Assertions.assertThat(bodies).hasSize(996)
-        Assertions.assertThat(bodies).contains("<html>www.twitter.com</html>", "<html>www.aol.com</html>", "<html>www.mozilla.org</html>")
+        assertThat(bodies).hasSize(996)
+        assertThat(bodies).contains("<html>www.twitter.com</html>", "<html>www.aol.com</html>", "<html>www.mozilla.org</html>")
+    }
+
+    @Test
+    fun shouldDownloadAllUrls() {
+        val urls = Urls.all()
+
+        var bodies = urls
+                .flatMap { url ->
+                    UrlDownloader
+                            .downloadAsync(url, Schedulers.io())
+                            .map { html -> Pair(url.toURI(), html) }
+                }
+                .toMap({ pair -> pair.first }, { pair -> pair.second })
+                .blockingGet()
+
+        assertThat(bodies).hasSize(996)
+        assertThat(bodies).containsEntry(URI("http://www.twitter.com"), "<html>www.twitter.com</html>")
+        assertThat(bodies).containsEntry(URI("http://www.aol.com"), "<html>www.aol.com</html>")
+        assertThat(bodies).containsEntry(URI("http://www.mozilla.org"), "<html>www.mozilla.org</html>")
     }
 }
--- src/test/java/io/github/gianpamx/rxkotlin/util/UrlDownloader.kt
+++ src/test/java/io/github/gianpamx/rxkotlin/util/UrlDownloader.kt
@@ -1,6 +1,7 @@
 package io.github.gianpamx.rxkotlin.util
 
 import io.reactivex.Flowable
+import io.reactivex.Scheduler
 import org.slf4j.LoggerFactory
 import java.net.URL
 import java.time.Duration
@@ -10,6 +11,8 @@ object UrlDownloader {
     private val log = LoggerFactory.getLogger(UrlDownloader::class.java)
 
     fun download(url: URL): Flowable<String> = Flowable.fromCallable<String> { downloadBlocking(url) }
+
+    fun downloadAsync(url: URL, scheduler: Scheduler): Flowable<String> = download(url).subscribeOn(scheduler)
 
     fun downloadBlocking(url: URL): String {
         log.trace("Downloading: {}", url)
